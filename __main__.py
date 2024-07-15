6cbeb67ca6e99eda639caa882b9eef0eabc7ef29
Operator <039kirill@gmail.com>
2024-07-15 18:17:58 +0200

    End(idk)

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils import executor
 from env import BOT_TOKEN
 from Answers import *
 storage = MemoryStorage()
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(storage=storage)
 
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
